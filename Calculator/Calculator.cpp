23fd059a46fa5ed9cc702467e50cd85f096311a7
Anna Sobanska <ania.webdev@gmail.com>
2021-09-09 10:59:48 +0200

    remove functions to one file

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 
 void simpleCalculator();
 void sweetsCalculator();
@@ -6,17 +7,17 @@ void bmiCalculator();
 void avgCalculator();
 void multiTable();
 
-
-int main() {
+int main() 
+{
 	int userChoice;
-	std::cout << "**My first calculator**" << std::endl;
-	std::cout << "Please, select a calculator:" << std::endl;
-	std::cout << "1. Simple calculator" << std::endl;
-	std::cout << "2. Sweets calculator" << std::endl;
-	std::cout << "3. BMI calculator" << std::endl;
-	std::cout << "4. Average calculator" << std::endl;
-	std::cout << "5. Multiplication table" << std::endl;
-	std::cin >> userChoice;
+	cout << "**My first calculator**" << endl;
+	cout << "Please, select a calculator:" << endl;
+	cout << "1. Simple calculator" << endl;
+	cout << "2. Sweets calculator" << endl;
+	cout << "3. BMI calculator" << endl;
+	cout << "4. Average calculator" << endl;
+	cout << "5. Multiplication table" << endl;
+	cin >> userChoice;
 	system("cls");
 	
 	switch (userChoice)
@@ -37,8 +38,109 @@ int main() {
 		multiTable();
 		break;
 	default:
-		std::cout << "Not valid operation!" << std::endl;
+		cout << "Not valid operation!" << endl;
 	}
 
 	system("pause>0");
+}
+
+void simpleCalculator()
+{
+	float num1, num2;
+	char operation;
+	cout << "Please, enter numbers and operator eg. 2+2" << endl;
+	cin >> num1 >> operation >> num2;
+
+	switch (operation)
+	{
+	case '+':
+		cout << num1 + num2 << endl;
+		break;
+	case '-':
+		cout << num1 - num2 << endl;
+		break;
+	case '*':
+		cout << num1 * num2 << endl;
+		break;
+	case '/':
+		if (num2 == 0) cout << "Don't divide by 0!";
+		else cout << num1 / num2 << endl;
+		break;
+	case '%':
+		bool isNum1Int, isNum2Int;
+		isNum1Int = ((int)num1 == num1);
+		isNum2Int = ((int)num2 == num2);
+
+		if (isNum1Int && isNum2Int)
+			cout << (int)num1 % (int)num2 << endl;
+		else
+			cout << "Not valid!";
+		break;
+	default:
+		cout << "Not valid operation!" << endl;
+	}
+}
+
+void sweetsCalculator()
+{
+	int sweets, students, sweetsPerStudent, sweetsForYou;
+
+	cout << "How many sweets did you buy? ";
+	cin >> sweets;
+
+	cout << "How many studenst is in your class? ";
+	cin >> students;
+
+	sweetsPerStudent = sweets / (students - 1);
+
+	sweetsForYou = sweets % (students - 1);
+
+	cout << "You have " << sweetsPerStudent << " sweets for one student and " << sweetsForYou << " sweets for you." << endl;
+}
+
+void bmiCalculator()
+{
+	float weight, height, bmi;
+
+	cout << "Enter your weight(kg), height(m): ";
+	cin >> weight >> height;
+
+	bmi = weight / (height * height);
+
+	if (bmi < 18.5)
+		cout << "You are underweight. Buy more sweets!" << endl;
+	else if (bmi > 25)
+		cout << "You are overweight. You eat too much sweets!" << endl;
+	else
+		cout << "You are normal weight." << endl;
+
+	cout << "Your BMI is " << bmi;
+}
+
+void avgCalculator() 
+{
+
+	int grade, sum = 0;
+
+	for (int i = 0; i < 3; i++) {
+
+		do {
+			cout << "Enter grade " << i + 1 << ": ";
+			cin >> grade;
+		} while (grade < 1 || grade > 5);
+		sum += grade;
+	}
+
+	cout << "Sum = " << sum << endl;
+	cout << "Avg grade = " << sum / 3.0 << endl;
+}
+
+void multiTable() 
+{
+	for (int i = 1; i <= 10; i++) {
+		for (int j = 1; j <= 10; j++) {
+			cout << i << " = " << i * j << endl;
+		}
+		cout << endl;
+	}
 }
